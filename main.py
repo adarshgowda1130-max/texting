8bd26ddac99a412de22a7e437969891ea3ad7aa7
Adarsha M P <adarshgowda1130@gmail.com>
2026-09-22 12:13:02 +0530

    user id created

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("today we gonna leaarn more about requests")
+import requests
+request_to_pixela={
+  "token":"Password@1208",
+  "username":"adhiiii",
+  "agreeTermsOfService":"yes",
+  "notMinor":"yes"
+}
+post=requests.post(url="https://pixe.la/v1/users",json=request_to_pixela)
+print(post.text)
