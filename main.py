8e2ed576f818526be7f623e395fa5b3afe6e44a6
Adarsha M P <adarshgowda1130@gmail.com>
2026-09-22 16:34:10 +0530

    final project

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import requests
+from datetime import date
+today=date.today().strftime("%Y%m%d")
+USERNAME="adhi17"
+TOKEN="Password@1208"
+GRAPH_ID="graph12"
 pixela_endpoint="https://pixe.la/v1/users"
 request_to_pixela={
-  "token":"Password@1208",
-  "username":"adhiiii",
+  "token":TOKEN,
+  "username":USERNAME,
   "agreeTermsOfService":"yes",
   "notMinor":"yes"
 }
-# post=requests.post(url=pixela_endpoint,json=request_to_pixela)
-# print(post.text)
-graph_endpoint=f"{pixela_endpoint}/adhiiii/graphs"
-graph_params={
+# acc_post=requests.post(url=pixela_endpoint,json=request_to_pixela)
+# print(acc_post.text)
+graph_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_parms={
     "timezone":"Asia/Kolkata",
     "color":"sora",
     "unit":"minutes",
     "type":"int",
     "name":"first_graph",
-    "id":"adarsh12"
+    "id":GRAPH_ID
 }
-graph_header={
-    "X-USER-TOKEN":"Password@1208"
+headers={
+    "X-USER-TOKEN":TOKEN
 }
-graph_post=requests.post(url=graph_endpoint,json=graph_params,headers=graph_header)
-print(graph_post.text)
+graph=requests.post(url=graph_endpoint,json=graph_parms,headers=headers)
+# print(graph.text)
+add_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+add_params={
+    "date":today,
+    "quantity":"45",
+}
+added=requests.post(url=add_endpoint,json=add_params,headers=headers)
+#print(added.text)
+update_endpoint=f"{graph_endpoint}/{GRAPH_ID}"
+update_parmas={
+    "unit":"12"
+}
+update=requests.put(url=update_endpoint,json=update_parmas,headers=headers)
+print(update.text)
